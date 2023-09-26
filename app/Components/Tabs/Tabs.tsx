c7da55d0688ad0130aa87619a228f6f07470a556
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-09-26 05:33:29 +0100

    FEAT: added tabs components

--- app/Components/Tabs/Tabs.tsx
+++ app/Components/Tabs/Tabs.tsx
@@ -1,5 +1,54 @@
-import styles from "./Tabs.module.css"
+"use client"
+import { Box, Flex } from "@mantine/core"
+import classes from "./Tabs.module.css"
+import Link from "next/link"
+import cx from "clsx"
+import { useState } from "react"
+
+const Tabs = [
+	{
+		label: "Information",
+		href: "#",
+	},
+	{
+		label: "Customize",
+		href: "#",
+	},
+	{
+		label: "Testers",
+		href: "#",
+	},
+	{
+		label: "Agreements",
+		href: "#",
+	},
+]
 
 export default function CustomTabs() {
-	return <></>
+	const [active, setActive] = useState("Information")
+	const items = Tabs.map(tab => (
+		<Box
+			key={tab.label}
+			component={Link}
+			href={tab.href}
+			onClick={event => {
+				event.preventDefault()
+				setActive(tab.label)
+			}}
+			className={cx(classes.link, {
+				[classes.linkActive]: active === tab.label,
+			})}
+		>
+			{tab.label}
+		</Box>
+	))
+	return (
+		<Flex
+			gap='1.6rem'
+			direction='column'
+			className={classes.group}
+		>
+			{items}
+		</Flex>
+	)
 }
