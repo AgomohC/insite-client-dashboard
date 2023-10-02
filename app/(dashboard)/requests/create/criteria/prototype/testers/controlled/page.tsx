ab3287ad462063f4c5e2eefc7319a0e3e2d9e6ba
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-10-02 01:58:16 +0100

    completed testers pag

--- app/(dashboard)/requests/create/criteria/prototype/testers/controlled/page.tsx
+++ app/(dashboard)/requests/create/criteria/prototype/testers/controlled/page.tsx
@@ -1,12 +1,44 @@
-import Link from "next/link"
-
+import CardLayoutWithHead from "../../../../components/CardLayoutWithHeader"
+import { Flex, Text } from "@mantine/core"
+import styles from "../../../../components/styles.module.css"
+import { Controlled, Arrow } from "@/public/icons"
+import DragAndDrop from "@/app/Components/DragAndDrop/DragAndDrop"
+import CustomButton from "@/app/Components/Button/Button"
 export default function Page() {
 	return (
-		<>
-			<Link href='/requests/create/prototype/nda'>Agreements</Link>
-
-			<h1>controlled targeted testers</h1>
-		</>
+		<CardLayoutWithHead
+			type='prototype'
+			headerText='Choose Testers'
+			href='/requests/create/criteria/prototype/testers'
+		>
+			<Flex
+				gap='1.6rem'
+				direction='column'
+				className={styles.targetLayout}
+			>
+				<Flex
+					className={styles.testersLayout}
+					direction='column'
+				>
+					<Flex
+						className={styles.testersHead}
+						gap='1.6rem'
+						align='center'
+					>
+						<Controlled />
+						<Text className={styles.testersHeadText}>
+							Controlled Targeted Testers
+						</Text>
+					</Flex>
+					<Flex
+						className={styles.testersBody}
+						direction='column'
+						gap='2.4rem'
+						p='2.4rem'
+					></Flex>
+				</Flex>
+			</Flex>
+		</CardLayoutWithHead>
 	)
 }
 
