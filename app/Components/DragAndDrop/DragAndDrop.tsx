4194f32e04eae952522336b59488c021cb3dc8b7
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-10-02 12:43:33 +0100

    dropzone fixes

--- app/Components/DragAndDrop/DragAndDrop.tsx
+++ app/Components/DragAndDrop/DragAndDrop.tsx
@@ -2,7 +2,12 @@
 import cx from "clsx"
 import styles from "./DragAndDrop.module.css"
 import "@mantine/dropzone/styles.css"
-import { Dropzone, DropzoneProps, MIME_TYPES } from "@mantine/dropzone"
+import {
+	Dropzone,
+	MIME_TYPES,
+	IMAGE_MIME_TYPE,
+	FileWithPath,
+} from "@mantine/dropzone"
 import { Box, Flex, Text, Image as MantineImage } from "@mantine/core"
 import Image from "next/image"
 import sketch from "@/public/images/sketch.png"
@@ -11,15 +16,22 @@ import design from "@/public/images/design.png"
 import IconImage from "@/public/images/image.svg"
 import Csv from "@/public/images/csv.svg"
 import { CSSProperties } from "react"
-
-MIME_TYPES.csv
-
-interface IProps extends Partial<DropzoneProps> {
+interface IProps {
 	type: "csv" | "design" | "image"
 	fileType?: Array<string>
+	onChange: (e: FileWithPath[]) => void
+	maxSize: number
+	title: string
+	className?: string
 }
 
-export default function DragAndDrop({ type, title, className }: IProps) {
+export default function DragAndDrop({
+	type,
+	title,
+	className,
+	onChange,
+	maxSize,
+}: IProps) {
 	return (
 		<Flex
 			direction='column'
@@ -27,11 +39,23 @@ export default function DragAndDrop({ type, title, className }: IProps) {
 		>
 			{title ? <Text className={styles.dropTitle}>{title}</Text> : null}
 			<Dropzone
-				onDrop={e => console.log(e)}
+				onDrop={e => {
+					onChange(e)
+				}}
+				onReject={files => {
+					console.log(files)
+				}}
 				classNames={{
 					root: cx(styles.root, { [styles.rootCsv]: type === "csv" }),
 				}}
 				className={className}
+				maxFiles={1}
+				maxSize={maxSize}
+				accept={
+					type === "csv"
+						? [MIME_TYPES.csv, MIME_TYPES.xlsx, MIME_TYPES.xls]
+						: IMAGE_MIME_TYPE
+				}
 			>
 				<Flex
 					w='100%'
@@ -45,140 +69,141 @@ export default function DragAndDrop({ type, title, className }: IProps) {
 						<></>
 					</Dropzone.Reject>
 					<Dropzone.Idle>
-						<Flex
-							gap='0.8rem'
-							direction='column'
-							justify='center'
-							align='center'
-						>
-							{type === "csv" ? (
-								<Box
-									className={cx(styles.icon, {
-										[styles.iconExcel]: type === "csv",
-									})}
-								>
-									<Csv />
-								</Box>
-							) : null}
-
-							{type === "image" ? (
-								<Box className={styles.icon}>
-									<IconImage />
-								</Box>
-							) : null}
-
-							{type === "design" ? (
-								<Flex className={styles.iconGroup}>
-									<Box
-										className={cx(styles.icon, styles.skew)}
-										style={
-											{
-												"--skew-deg": "-45deg",
-												"--img-skew-deg": "45deg",
-											} as CSSProperties
-										}
-									>
-										<MantineImage
-											component={Image}
-											alt='sketch'
-											src={sketch.src}
-											width={45}
-											height={36}
-											className={styles.iconImg}
-										/>
-									</Box>
-									<Box
-										className={cx(styles.icon, styles.skew)}
-										style={
-											{
-												"--skew-deg": "0deg",
-												"--img-skew-deg": "0deg",
-											} as CSSProperties
-										}
-									>
-										<MantineImage
-											component={Image}
-											alt='figma'
-											src={figma.src}
-											width={45}
-											height={36}
-											className={styles.iconImg}
-										/>
-									</Box>
-									<Box
-										className={cx(styles.icon, styles.skew)}
-										style={
-											{
-												"--skew-deg": "45deg",
-												"--img-skew-deg": "-45deg",
-											} as CSSProperties
-										}
-									>
-										<MantineImage
-											component={Image}
-											alt='design'
-											src={design.src}
-											width={45}
-											height={36}
-											className={styles.iconImg}
-										/>
-									</Box>
-								</Flex>
-							) : null}
-
-							{type === "csv" ? (
-								<Flex
-									gap='1.2rem'
-									direction='column'
-									align='center'
-									justify='center'
-								>
-									<Text className={styles.dropText}>
-										Drag & Drop or{" "}
-										<span className={styles.browse}>Browse</span> your
-										files
-									</Text>
-
-									<Text className={styles.dropSubtext}>
-										Max. file size: 2MB
-									</Text>
-								</Flex>
-							) : null}
-							{type === "design" ? (
-								<Flex
-									gap='1.2rem'
-									direction='column'
-									align='center'
-									justify='center'
-								>
-									<Text className={cx(styles.dropText, styles.notCsv)}>
-										Drag and drop a Design File
-									</Text>
-
-									<Box className={styles.dropSubtextBox}>
-										Upload design file
-									</Box>
-								</Flex>
-							) : null}
-
-							{type === "image" ? (
-								<Flex
-									gap='1.2rem'
-									direction='column'
-									align='center'
-									justify='center'
-								>
-									<Text className={cx(styles.dropText, styles.notCsv)}>
-										Drag and drop a Design Image
-									</Text>
-
-									<Box className={styles.dropSubtextBox}>
-										Upload design
-									</Box>
-								</Flex>
-							) : null}
-						</Flex>
+						<></>
 					</Dropzone.Idle>
+					<Flex
+						gap='0.8rem'
+						direction='column'
+						justify='center'
+						align='center'
+					>
+						{type === "csv" ? (
+							<Box
+								className={cx(styles.icon, {
+									[styles.iconExcel]: type === "csv",
+								})}
+							>
+								<Csv />
+							</Box>
+						) : null}
+
+						{type === "image" ? (
+							<Box className={styles.icon}>
+								<IconImage />
+							</Box>
+						) : null}
+
+						{type === "design" ? (
+							<Flex className={styles.iconGroup}>
+								<Box
+									className={cx(styles.icon, styles.skew)}
+									style={
+										{
+											"--skew-deg": "-45deg",
+											"--img-skew-deg": "45deg",
+										} as CSSProperties
+									}
+								>
+									<MantineImage
+										component={Image}
+										alt='sketch'
+										src={sketch.src}
+										width={45}
+										height={36}
+										className={styles.iconImg}
+									/>
+								</Box>
+								<Box
+									className={cx(styles.icon, styles.skew)}
+									style={
+										{
+											"--skew-deg": "0deg",
+											"--img-skew-deg": "0deg",
+										} as CSSProperties
+									}
+								>
+									<MantineImage
+										component={Image}
+										alt='figma'
+										src={figma.src}
+										width={45}
+										height={36}
+										className={styles.iconImg}
+									/>
+								</Box>
+								<Box
+									className={cx(styles.icon, styles.skew)}
+									style={
+										{
+											"--skew-deg": "45deg",
+											"--img-skew-deg": "-45deg",
+										} as CSSProperties
+									}
+								>
+									<MantineImage
+										component={Image}
+										alt='design'
+										src={design.src}
+										width={45}
+										height={36}
+										className={styles.iconImg}
+									/>
+								</Box>
+							</Flex>
+						) : null}
+
+						{type === "csv" ? (
+							<Flex
+								gap='1.2rem'
+								direction='column'
+								align='center'
+								justify='center'
+							>
+								<Text className={styles.dropText}>
+									Drag & Drop or{" "}
+									<span className={styles.browse}>Browse</span> your
+									files
+								</Text>
+
+								<Text className={styles.dropSubtext}>
+									Max. file size: {maxSize / 1024 ** 2} MB
+								</Text>
+							</Flex>
+						) : null}
+						{type === "design" ? (
+							<Flex
+								gap='1.2rem'
+								direction='column'
+								align='center'
+								justify='center'
+							>
+								<Text className={cx(styles.dropText, styles.notCsv)}>
+									Drag and drop a Design File
+								</Text>
+
+								<Box className={styles.dropSubtextBox}>
+									Upload design file
+								</Box>
+							</Flex>
+						) : null}
+
+						{type === "image" ? (
+							<Flex
+								gap='1.2rem'
+								direction='column'
+								align='center'
+								justify='center'
+							>
+								<Text className={cx(styles.dropText, styles.notCsv)}>
+									Drag and drop a Design Image
+								</Text>
+
+								<Box className={styles.dropSubtextBox}>
+									Upload design
+								</Box>
+							</Flex>
+						) : null}
+					</Flex>
 				</Flex>
 			</Dropzone>
 			<Flex>
