7add8d3607a9715ff97aea27ad7d14b6cfebf525
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-10-02 02:26:05 +0100

    feat: added appropriate z index

--- app/(dashboard)/requests/create/criteria/prototype/testers/insite/page.tsx
+++ app/(dashboard)/requests/create/criteria/prototype/testers/insite/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import CardLayoutWithHead from "../../../../components/CardLayoutWithHeader"
 import styles from "../../../../components/styles.module.css"
-import { Flex, Stack, Text, Drawer } from "@mantine/core"
+import { Flex, Stack, Text, Drawer, ScrollArea } from "@mantine/core"
 import { LogoSmall, New } from "@/public/icons"
 import { useDisclosure } from "@mantine/hooks"
 import avatar1 from "@/public/images/avatar-1.png"
@@ -87,8 +87,50 @@ export default function Page() {
 				onClose={close}
 				title='Testers'
 				position='right'
-				withinPortal={false}
-			></Drawer>
+				scrollAreaComponent={ScrollArea.Autosize}
+				classNames={{
+					inner: styles.drawerInner,
+					content: styles.drawerContent,
+				}}
+			>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore at
+				pariatur eligendi molestiae recusandae, maiores deleniti, aspernatur
+				necessitatibus impedit quibusdam nam aliquam quo repellat beatae
+				molestias consectetur tempora tenetur? Tempora, quibusdam quidem
+				tenetur asperiores odit ratione libero explicabo vero ipsum ullam
+				praesentium fuga pariatur quasi dolor dignissimos! Magni obcaecati
+				sunt asperiores eligendi voluptatibus hic quisquam ratione adipisci,
+				libero aliquam debitis optio animi harum esse ad voluptates
+				exercitationem quidem soluta autem repellat fugit quos cum
+				repellendus quibusdam. Officiis temporibus suscipit maiores omnis?
+				Ipsa cum, libero adipisci id quisquam eaque accusamus, magni
+				blanditiis, consequatur asperiores suscipit voluptate? Veniam
+				aliquid, nam tempore, facilis, corporis laboriosam aut asperiores
+				vero ducimus officia sit praesentium cumque? Corporis illum quod
+				blanditiis explicabo esse repellat ea aut aspernatur aliquam
+				excepturi ab veritatis vel, impedit atque deleniti nostrum
+				temporibus! Earum incidunt rem adipisci harum mollitia repellendus
+				commodi quisquam aperiam. Nulla nihil porro quo corrupti maxime eos
+				doloremque quisquam assumenda excepturi quasi. Sequi excepturi
+				voluptatum accusantium doloribus blanditiis doloremque ipsa,
+				laudantium sint corporis sunt similique, fugit non placeat animi
+				odio magnam hic in tenetur nihil autem adipisci minus minima.
+				Asperiores, reprehenderit! Magni officiis provident ad, quas
+				perferendis maxime adipisci doloribus repudiandae dolore, facilis
+				fugit sapiente est ut at porro voluptatibus repellat sunt veritatis
+				magnam similique. Modi, nihil ratione omnis facilis, unde officiis
+				tempora, autem eius dolore consequatur veniam perspiciatis quia.
+				Debitis non magni consectetur eius ratione inventore enim incidunt
+				vel, voluptates magnam aliquam, est mollitia nobis obcaecati
+				cupiditate officia dolore voluptatem, facere quibusdam veritatis!
+				Aut iure possimus hic. Doloremque iusto cum in eveniet dolore nulla
+				perferendis officiis, asperiores officia magni tempore ullam
+				veritatis quod at, dignissimos quos qui assumenda deserunt labore
+				doloribus beatae totam mollitia? Molestias pariatur iusto omnis
+				aliquam non dolore, blanditiis eius consectetur dolorum possimus
+				fugit laborum expedita doloribus veritatis nobis itaque? Quasi
+				quisquam dignissimos ut aspernatur architecto.
+			</Drawer>
 		</CardLayoutWithHead>
 	)
 }
