dfd44359a76c5aa0574b82fd6cb79b0147f6fa81
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-10-09 04:34:48 +0100

    modified prototype request types

--- app/(dashboard)/requests/create/criteria/prototype/types.ts
+++ app/(dashboard)/requests/create/criteria/prototype/types.ts
@@ -1,30 +1,38 @@
 import * as z from "zod"
 
 export const basePrototypeSchema = z.object({
-	processName: z.string().min(1, { message: "Process name must be a string" }),
+	processName: z
+		.string({ required_error: "Process name must be a string" })
+		.min(1, { message: "Process name must be a string" }),
 	prototypeLink: z
-		.string()
+		.string({ required_error: "Prototype Link is required" })
 		.min(1, { message: "Prototype Link must be a string" })
 		.url({ message: "Prototype Link must be a valid url" }),
 	status: z.enum(["complete", "incomplete"]),
-	requestType: z.enum(["Tasks", "Feedbacks"]),
+	requestType: z
+		.string({ required_error: "Request type is required" })
+		.min(1, { message: "Request type is required" }),
+
 	prototypeType: z.enum(["figma", "sketch"]),
 })
 export type BasePrototypeSchemaType = z.infer<typeof basePrototypeSchema>
 
 export const TaskSchema = z.object({
 	instructions: z.array(z.string()),
-	type: z.enum(["timed", "recorded"]),
+	type: z
+		.array(z.enum(["timed", "recorded"]))
+		.max(2)
+		.min(1),
 })
 
 export type TaskSchemaType = z.infer<typeof TaskSchema>
 
 export const ratingsFeedbackSchema = z.object({
-	feedbackType: z.literal("Rating"),
+	feedbackType: z.literal("Scale"),
 	questions: z.array(
 		z.object({
 			question: z.string(),
-			ratingType: z.enum(["number", "star"]),
+			ratingType: z.enum(["number", "star", "experience", ""]),
 			lowerLabel: z.string(),
 			upperLabel: z.string(),
 			scale: z.enum(["5", "10", "15", "20"]),
@@ -35,12 +43,10 @@ export type ratingsFeedbackSchemaType = z.infer<typeof ratingsFeedbackSchema>
 
 export const interviewsFeedbackSchema = z.object({
 	feedbackType: z.literal("Interview"),
-	questions: z.array(
-		z.object({
-			recordingOption: z.enum(["video", "audio"]),
-			question: z.string(),
-		})
-	),
+	questions: z.array(z.string({ required_error: "Question is required" })),
+	recordingOption: z.string({
+		required_error: "Recording option is required",
+	}),
 })
 
 export type interviewsFeedbackSchemaType = z.infer<
