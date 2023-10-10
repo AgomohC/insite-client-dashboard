f3b9b9961bdc70a0721228a9c5421518237ff20c
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-10-10 17:17:01 +0100

    feat: modified next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	webpack(config) {
+		/**
+		 * ? SVGR SETUP
+		 */
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ["@svgr/webpack"],
 		})
+		/**
+		 * ?  REACT PDF SETUP
+		 */
+		config.resolve.alias.canvas = false
 
 		return config
 	},
